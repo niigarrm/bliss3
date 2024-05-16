94e3345ff8ba753de4efbf067a2e55a603367e06
Evaldas <81704689+32Butterfly@users.noreply.github.com>
2024-05-16 12:37:42 +0300

    Refactoring the code/ making additional features
    Added so the date aka the year/month/day for each of the Timezone. Added so the button has a hover animation. Breaking down  code into more methods

--- Source/TimeZoneApp.java
+++ Source/TimeZoneApp.java
@@ -9,16 +9,16 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
 import javafx.geometry.Pos;
-import javafx.scene.text.Font;
-import javafx.stage.Stage;
-import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
-import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+import javafx.scene.paint.Color;
 import javafx.util.Duration;
-
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -27,53 +27,44 @@ public class TimeZoneApp extends Application {
     private String countryName;
     @Override
     public void start(Stage primaryStage) {
-        /*
-        The following lines until fetch button are for creating our fields that will be displayed in our GUI.
-        We set their text position, text size, font, size and background colour.
-         */
-        Font font = new Font("Impact", 60);
-        Label team = new Label("Made by team: Bliss3");
-        Font fontForTeam = new Font("Impact", 18);
-        team.setBackground(new Background(new BackgroundFill(Color.PINK, null, null)));
-        team.setFont(fontForTeam);
 
-        Label countryTime = new Label("Local time: ");
-        countryTime.setBackground(new Background(new BackgroundFill(Color.PINK, null, null)));
-        countryTime.setFont(font);
-        countryTime.setAlignment(Pos.CENTER);
+        //create the fonts and background
+        Font font = new Font("Impact", 52);
+        Font inputFont = Font.font("Segoe UI", 18);
+        Font labelFont = Font.font("Segoe UI", FontWeight.BOLD, 18);
+        Background background = new Background(new BackgroundFill(Color.PINK, null, null));
 
-        TextField countryInput = new TextField();
-        countryInput.setPrefSize(250, 15);
-
-        Button fetchButton = new Button("Get Time");
-        fetchButton.hoverProperty();
-
-        String localTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"));
-        Label resultDisplay = new Label(localTime);
-        resultDisplay.setBackground(new Background(new BackgroundFill(Color.PINK, null, null)));
-        resultDisplay.setFont(font);
-        resultDisplay.setAlignment(Pos.CENTER);
+        //create all of our GUI components
+        Label team = createTeamLabel(background);
+        Label countryTime = createCountryTimeLabel(background, font);
+        TextField countryInput = createCountryInput(inputFont);
+        Button fetchButton = createFetchButton(labelFont);
+        Label date = createDateLabel(background);
+        Label resultDisplay = createResultDisplayLabel(font, background);
 
         //Whenever the button is pressed we take everything from the input box and try to get api result from it.
-        fetchButton.setOnAction(e -> {
-            fetchTimeData(countryInput, resultDisplay, countryTime);
-        });
+        fetchButton.setOnAction(e -> fetchTimeData(countryInput, resultDisplay, countryTime, date));
 
+        //Align our components in the GUI
         HBox teamName = new HBox(20, team);
         teamName.setAlignment(Pos.TOP_RIGHT);
-        teamName.setPadding(new Insets(5));
+        teamName.setPadding(new Insets(10));
 
         HBox searchBar = new HBox(20, countryInput, fetchButton);
         searchBar.setAlignment(Pos.TOP_CENTER);
 
-        VBox resultBox = new VBox(10, countryTime, resultDisplay);
+        HBox dateTime = new HBox(5, date);
+        dateTime.setAlignment(Pos.BASELINE_RIGHT);
+        dateTime.setPadding(new Insets(0, 60, 0, 60));
+
+        VBox resultBox = new VBox(10, dateTime, countryTime, resultDisplay);
         resultBox.setAlignment(Pos.CENTER);
 
-        VBox layoutSearchAndResult = new VBox(135, searchBar, resultBox);
-        layoutSearchAndResult.setBackground(new Background(new BackgroundFill(Color.PINK, null, null)));
+        VBox layoutSearchAndResult = new VBox(95, searchBar, resultBox);
+        layoutSearchAndResult.setBackground(background);
 
         VBox layout = new VBox(5, teamName, layoutSearchAndResult);
-        layout.setBackground(new Background(new BackgroundFill(Color.PINK, null, null)));
+        layout.setBackground(background);
 
         Scene scene = new Scene(layout, 780, 500);
 
@@ -84,15 +75,81 @@ public class TimeZoneApp extends Application {
         primaryStage.show();
     }
 
-    private void fetchTimeData(TextField countryInput, Label resultDisplay, Label countryTime) {
+    private Label createDateLabel(Background background) {
+        String localDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        Label date = new Label("Date: " + localDate);
+        Font dateFont = new Font("Impact", 26);
+        date.setFont(dateFont);
+        date.setBackground(background);
+        return date;
+    }
+
+    private TextField createCountryInput(Font inputFont) {
+        TextField countryInput = new TextField();
+        countryInput.setPrefSize(250, 36);
+        countryInput.setStyle("-fx-background-color: #f0f0f0; -fx-text-fill: #333333;");
+        countryInput.setFont(inputFont);
+        return countryInput;
+    }
+
+    private Button createFetchButton(Font font) {
+        Button fetchButton = new Button("Get Time");
+        fetchButton.setStyle("-fx-background-color: #4CAF50; -fx-text-fill: white;");
+        fetchButton.setFont(font);
+        setFetchButtonHoverEffect(fetchButton);
+        return fetchButton;
+    }
+
+    private Label createResultDisplayLabel(Font font, Background background) {
+        String localTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+        Label resultDisplay = new Label(localTime);
+        resultDisplay.setBackground(background);
+        resultDisplay.setFont(font);
+        return resultDisplay;
+    }
+
+    private Label createCountryTimeLabel(Background background, Font font) {
+        Label countryTime = new Label("Local time: ");
+        countryTime.setBackground(background);
+        countryTime.setFont(font);
+        return countryTime;
+    }
+
+    private Label createTeamLabel(Background background) {
+        Label team = new Label("Made by team: Bliss3");
+        Font fontForTeam = new Font("Impact", 18);
+        team.setBackground(background);
+        team.setFont(fontForTeam);
+        return team;
+    }
+
+    private void setFetchButtonHoverEffect(Button fetchButton) {
+        fetchButton.hoverProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue) {
+                // Mouse entered button
+                fetchButton.setStyle("-fx-background-color: #66BB6A; -fx-text-fill: white;");
+                fetchButton.setScaleX(1.15); // Increase size by 15% horizontally
+                fetchButton.setScaleY(1.15); // Increase size by 15% verticall
+            } else {
+                // Mouse exited button
+                fetchButton.setStyle("-fx-background-color: #4CAF50; -fx-text-fill: white;");
+                fetchButton.setScaleX(1.0); // Reset size
+                fetchButton.setScaleY(1.0); // Reset size
+            }
+        });
+    }
+
+    private void fetchTimeData(TextField countryInput, Label resultDisplay, Label countryTime, Label date) {
         countryName = countryInput.getText(); //take text from the input box
         String result = ApiClient.getTimeZoneData(countryName); //fecth data from api
         int startIndex = result.indexOf("datetime") + 11; // Find the index where the datetime value starts
         int endIndex = result.indexOf("+"); // Find the index where the timezone offset starts
         String datetime = result.substring(startIndex, endIndex); // Extract the datetime string
+        String countryDate = datetime.split("T")[0];
         String time = datetime.split("T")[1].split("\\.")[0]; //extract only hours/minutes/seconds
 
         resultDisplay.setText(time); //display time
+        date.setText(countryName + " Date: " + countryDate);
         countryTime.setText(countryName + " time:"); //update from local time to user's input country
     }
 
@@ -108,11 +165,12 @@ public class TimeZoneApp extends Application {
                           String datetime = result.substring(startIndex, endIndex);
                           String time = datetime.split("T")[1].split("\\.")[0];
 
-                          resultDisplay.setText(time);
+                          resultDisplay.setText(time); //display time
                           countryTime.setText(countryName + " time:");
                      }
                   }
                   else{
+                      //If the api has not been used yet we update the local time
                       String currentTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"));
                       resultDisplay.setText(currentTime);
                   }
